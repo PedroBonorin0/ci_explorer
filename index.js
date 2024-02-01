fbba597bcae4bdb40dbaea3d97ced0de8707b7ad
Pedro Bonorino <72540958+PedroBonorin0@users.noreply.github.com>
2024-02-01 11:54:32 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-console.log('Will print env below')
-console.log(process.env.TEST_ENV)
-console.log('Env printed ^^')
+console.log('Will print env below');
+console.log(process.env.SUPER_SECRET);
+const ENV = process.env.ENVIRONMENT;
+console.log('Env= ', ENV);
+console.log('Env printed ^^');
