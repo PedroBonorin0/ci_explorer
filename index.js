17a6e124fb58e353c544eec3bdc24fad880d0e25
Pedro Bonorino <72540958+PedroBonorin0@users.noreply.github.com>
2024-02-01 11:59:30 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 console.log('Will print env below');
 console.log(process.env.SUPER_SECRET);
 const ENV = process.env.ENVIRONMENT;
-console.log('Env= ', ENV);
+console.log('Env= ', ENV, ' type is ', typeof ENV);
 console.log('Env printed ^^');
